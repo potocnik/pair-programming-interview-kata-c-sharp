5a7d564e4980eecf06dbf9c4afac20d19eaefca0
Hibri Marzook <hibri@hibri.net>
2016-01-11 13:57:41 +0000

    Add project

---
+++ src/Tests.Unit/Tests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Tests.Unit
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Should_be_true()
+        {
+            Assert.That(true, Is.EqualTo(false));
+        }
+    }
+}
